ebd6e4a5b41963aa308a8574d8304b05dfa30441
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-16 18:23:41 +0530

    Add DefaultConstructor program

---
+++ DefaultConstructor.java
@@ -0,0 +1,8 @@
+class DefaultConstructor {
+    DefaultConstructor() {
+        System.out.println("default constructor");
+    }
+    public static void main(String[] args) {
+        DefaultConstructor obj = new DefaultConstructor();
+    }
+}
