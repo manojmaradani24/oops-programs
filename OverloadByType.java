cda64192c5f5a2518904790b21715b845ae65fd9
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-20 14:42:37 +0530

    Add Rotate Array program

---
+++ OverloadByType.java
@@ -0,0 +1,12 @@
+class Main {
+    static void display(int a){
+        System.out.println("Integer value: " + a);
+    }
+    static void display(double a){
+        System.out.println("Double value: " + a);
+    }
+    public static void main(String[] args) {
+        display(10);
+        display(5.5);
+    }
+}
