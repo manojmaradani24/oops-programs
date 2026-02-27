2862faae9652acf020530c2853c2d338c2cfcad7
Manoj Maradani <manojmaradani24@gmail.com>
2026-02-27 15:21:34 +0530

    Add MultipleInterface program

---
+++ MultipleInterface.java
@@ -0,0 +1,19 @@
+interface Father {
+    void fatherProperty();
+}
+interface Mother {
+    void motherProperty();
+}
+class Child implements Father, Mother {
+    public void fatherProperty() {
+        System.out.println("Father has land");
+    }
+    public void motherProperty() {
+        System.out.println("Mother has gold");
+    }
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.fatherProperty();
+        c.motherProperty();
+    }
+}
