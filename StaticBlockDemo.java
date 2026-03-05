43aa8c119ffe46f53225314bf6b5d49e722c5a82
Manoj Maradani <manojmaradani24@gmail.com>
2026-03-05 13:20:14 +0530

    Add StaticBlockDemo program

---
+++ StaticBlockDemo.java
@@ -0,0 +1,8 @@
+public class StaticBlockDemo {
+    static {
+        System.out.println("Static block executed");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main method executed");
+    }
+}
