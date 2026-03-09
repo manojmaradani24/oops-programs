237c931494c4de2190bcf1f4324c8e120322b6e3
Manoj Maradani <manojmaradani24@gmail.com>
2026-03-09 13:41:11 +0530

    Add FinallyBlockDemo program

---
+++ FinallyBlockDemo.java
@@ -0,0 +1,17 @@
+public class FinallyBlockDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 5;
+            int b = 0;
+            int result = a / b;
+            System.out.println(result);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Exception occurred");
+        } 
+        finally {
+            System.out.println("Finally block always executes");
+        }
+        System.out.println("End of program");
+    }
+}
